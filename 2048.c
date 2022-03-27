4480a1727832359a816b33d3b28122f4323f5629
Harshit Gulgulia <78288704+HarshitGulgulia@users.noreply.github.com>
2022-03-27 22:41:53 +0530

    Update 2048.c

--- 2048.c
+++ 2048.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <conio.h>
-#include<ctype.h>
+#include <ctype.h>
 #include <windows.h>
 #include <process.h>
 
@@ -15,17 +15,21 @@
 #define ESC 57
 #define PR 2
 
-int Matrix[SIZE+1][SIZE+1]={0};
-int score=2048,last=0;
+int Matrix[SIZE + 1][SIZE + 1] = {0};
+int score = 2048, last = 0;
 
 void starting();
 void wait();
+void waitL();
+void vname();
 void Display();
 void print_ever();
 void Action(int);
 void Random_creator();
 void Starting_Random();
 int Temp_counter();
+void ending();
+void record();
 void Down();
 void Up();
 void Left();
@@ -39,21 +43,23 @@ void starting()
     printf(" ->  For wining this game any one box have value 2048\n\n");
     printf(" -> You have maximum 2048 try to win the game\n\n\n\n");
     printf("\t\t\t\t\t\tPRESS ANY KEY TO PLAY\n");
-    while(!kbhit());
+    while (!kbhit())
+        ;
 }
 
 void wait()
 {
     int i;
-    for(i=0;i<4500000;i++);
+    for (i = 0; i < 4500000; i++)
+        ;
 }
 
 printer(char s[])
 {
-    int i=0;
-    while(s[i]!='\0')
+    int i = 0;
+    while (s[i] != '\0')
     {
-        printf("%c",s[i]);
+        printf("%c", s[i]);
 
         i++;
     }
@@ -62,60 +68,61 @@ printer(char s[])
 void Action(int Arrow)
 {
 
-    switch(Arrow)
+    switch (Arrow)
     {
-        case UP:
-            {
-                Up();
-                break;
-            }
-        case DOWN:
-            {
-                Down();
-                break;
-            }
-        case LEFT:
-            {
-                Left();
-                break;
-            }
-        case RIGHT:
-            {
-                Right();
-                break;
-            }
-        default:
-            {
-                /*DO NOTHING */
-                return;
-            }
+    case UP:
+    {
+        Up();
+        break;
+    }
+    case DOWN:
+    {
+        /* logic here */
+        Down();
+        break;
+    }
+    case LEFT:
+    {
+        Left();
+        break;
+    }
+    case RIGHT:
+    {
+        Right();
+        break;
+    }
+    default:
+    {
+        /*DO NOTHING */
+        return;
+    }
     }
     score--;
-    Random_creator();
+    Random_creater();
     Display();
 }
 
-void Random_creator()
+void Random_creater()
 {
-    int temp1,temp2,add,i,j;
+    int temp1, temp2, add, i, j;
     srand(time(NULL));
-    temp1=rand()%SIZE;  // i
+    temp1 = rand() % SIZE; // i
 
     srand(time(NULL));
-    temp2=rand()%SIZE;  // j
+    temp2 = rand() % SIZE; // j
 
-    if((temp1+temp2)%2==0)
-        add=2;
+    if ((temp1 + temp2) % 2 == 0)
+        add = 2;
     else
-        add=4;
+        add = 4;
 
-    for(i=0;i<temp1;i++)
+    for (i = 0; i < temp1; i++)
     {
-        for(j=temp2;j<SIZE;j++)
+        for (j = temp2; j < SIZE; j++)
         {
-            if(Matrix[i][j]==0)
+            if (Matrix[i][j] == 0)
             {
-                Matrix[i][j]=add;
+                Matrix[i][j] = add;
                 return;
             }
         }
@@ -125,95 +132,157 @@ void Random_creator()
 void Display()
 {
 
-    int i,j;
+    int i, j;
     system("cls");
     print_ever();
-    for(i=0;i<SIZE;i++)
+    for (i = 0; i < SIZE; i++)
     {
-        printf("\t\t\t %c",BORDER);
-        for(j=0;j<SIZE;j++)
+        printf("\t\t\t %c", BORDER);
+        for (j = 0; j < SIZE; j++)
         {
-            if(Matrix[i][j]==0)
+            if (Matrix[i][j] == 0)
             {
                 printf("      ");
             }
             else
-                printf(" %4d ",Matrix[i][j]);
+                printf(" %4d ", Matrix[i][j]);
         }
-        printf("%c\n\t\t\t %c                        %c\n",BORDER,BORDER,BORDER);
+        printf("%c\n\t\t\t %c                        %c\n", BORDER, BORDER, BORDER);
     }
-    printf("\t\t\t %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n\n",BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER);
+    printf("\t\t\t %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n\n", BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER);
 }
 void print_ever()
 {
     printf("\n\n\n\t\t\t2048\n");
-    printf("\t\t\t\t\t\t\t\t STEPS LEFT  : %d\n\n\n\n",score);
-    printf("\t\t\t %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER);
+    printf("\t\t\t\t\t\t\t\t SCORE  : %d\n\n\n\n", score);
+    printf("\t\t\t %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER, BORDER);
+}
 
+void ending()
+{
+    /*here print prev high score and current score from file */
+    printf("\n\n\t\t\t\tYOUR SCORE is  : %d", score);
+    record();
+}
+void record()
+{
+    char plname[20], nplname[20], cha, c;
+    int i, j;
+    FILE *info;
+    info = fopen("record.txt", "a+");
+    getch();
+    system("cls");
+    printf("Enter your name\n");
+    scanf("%s", plname);
+    //************************
+    for (j = 0; plname[j] != '\0'; j++)
+    { // to convert the first letter after space to capital
+        nplname[0] = toupper(plname[0]);
+        if (plname[j - 1] == ' ')
+        {
+            nplname[j] = toupper(plname[j]);
+            nplname[j - 1] = plname[j - 1];
+        }
+        else
+            nplname[j] = plname[j];
+    }
+    nplname[j] = '\0';
+    //*****************************
+    // sdfprintf(info,"\t\t\tPlayers List\n");
+    fprintf(info, "Player Name :%s\n", nplname);
+    // for date and time
+
+    time_t mytime;
+    mytime = time(NULL);
+    fprintf(info, "Played Date:%s", ctime(&mytime));
+    //**************************
+    fprintf(info, "Score:%d\n", score); // call score to display score
+    // fprintf(info,"\nLevel:%d\n",10);//call level to display level
+    for (i = 0; i <= 50; i++)
+        fprintf(info, "%c", '_');
+    fprintf(info, "\n");
+    fclose(info);
+    printf("wanna see past records press 'y'\n");
+    cha = getch();
+    system("cls");
+    if (cha == 'y')
+    {
+        info = fopen("record.txt", "r");
+        do
+        {
+            putchar(c = getc(info));
+        } while (c != EOF);
+    }
+    fclose(info);
+    printf("\n\n\n\t\t\t\tPRESS ANY KEY TO EXIT\n");
+    while (!kbhit())
+        ;
+    system("attrib +h +s record.txt");
 }
 
 void Starting_Random()
 {
-    Matrix[3][1]=8;
-    Matrix[3][2]=32;
-    Matrix[3][3]=16;
-    Matrix[2][2]=8;
-    Matrix[2][3]=8;
-    Matrix[1][2]=8;
+    Matrix[3][1] = 8;
+    Matrix[3][2] = 32;
+    Matrix[3][3] = 16;
+    Matrix[2][2] = 8;
+    Matrix[2][3] = 8;
+    Matrix[1][2] = 8;
 
     Display();
 }
 
 int Temp_counter()
 {
-    int temp=0,i,j;
+    /* IT SHOULD FIND MAX VALUE FROM WHOLE MATRIX  */
+    int temp = 0, i, j;
 
-    for(i=0;i<SIZE;i++)
+    for (i = 0; i < SIZE; i++)
     {
-        for(j=0;j<SIZE;j++)
+        for (j = 0; j < SIZE; j++)
         {
-            if(Matrix[i][j]==2048)
-                return(1);
-            if(Matrix[i][j]==0)
-                temp=1;
+            if (Matrix[i][j] == 2048)
+                return (1);
+            if (Matrix[i][j] == 0)
+                temp = 1;
         }
     }
-    if(temp==1)
+    if (temp == 1)
     {
-        last=0;
-        return(-99);
+        last = 0;
+        return (-99);
     }
     else
     {
-        if(last==1)
+        if (last == 1)
         {
-            return(0);
+            return (0);
         }
-        last=1;
-        return(-99);
+        last = 1;
+        return (-99);
     }
 }
 
 void Down()
 {
-    int i,j;
-    for(j=0;j<SIZE;j++)
+    int i, j;
+    for (j = 0; j < SIZE; j++)
     {
-        i=2;
-        while(1)
+        i = 2;
+        while (1)
         {
-            while(Matrix[i][j]==0)
+            while (Matrix[i][j] == 0)
             {
-                if(i==-1)
+                if (i == -1)
                     break;
                 i--;
             }
-            if(i==-1)
+            if (i == -1)
                 break;
-            while(i<SIZE-1 && (Matrix[i+1][j]==0 || Matrix[i][j]==Matrix[i+1][j]))
+            while (i < SIZE - 1 && (Matrix[i + 1][j] == 0 || Matrix[i][j] == Matrix[i + 1][j]))
             {
-                Matrix[i+1][j]+=Matrix[i][j];
-                Matrix[i][j]=0;
+                Matrix[i + 1][j] += Matrix[i][j];
+                Matrix[i][j] = 0;
                 i++;
             }
             i--;
@@ -222,24 +291,24 @@ void Down()
 }
 void Up()
 {
-    int i,j;
-    for(j=0;j<SIZE;j++)
+    int i, j;
+    for (j = 0; j < SIZE; j++)
     {
-        i=1;
-        while(1)
+        i = 1;
+        while (1)
         {
-            while(Matrix[i][j]==0)
+            while (Matrix[i][j] == 0)
             {
-                if(i==SIZE)
+                if (i == SIZE)
                     break;
                 i++;
             }
-            if(i==SIZE)
+            if (i == SIZE)
                 break;
-            while(i>0 && (Matrix[i-1][j]==0 || Matrix[i][j]==Matrix[i-1][j]))
+            while (i > 0 && (Matrix[i - 1][j] == 0 || Matrix[i][j] == Matrix[i - 1][j]))
             {
-                Matrix[i-1][j]+=Matrix[i][j];
-                Matrix[i][j]=0;
+                Matrix[i - 1][j] += Matrix[i][j];
+                Matrix[i][j] = 0;
                 i--;
             }
             i++;
@@ -248,24 +317,24 @@ void Up()
 }
 void Right()
 {
-    int i,j;
-    for(i=0;i<SIZE;i++)
+    int i, j;
+    for (i = 0; i < SIZE; i++)
     {
-        j=2;
-        while(1)
+        j = 2;
+        while (1)
         {
-            while(Matrix[i][j]==0)
+            while (Matrix[i][j] == 0)
             {
-                if(j==-1)
+                if (j == -1)
                     break;
                 j--;
             }
-            if(j==-1)
+            if (j == -1)
                 break;
-            while(j<SIZE-1 && (Matrix[i][j+1]==0 || Matrix[i][j]==Matrix[i][j+1]))
+            while (j < SIZE - 1 && (Matrix[i][j + 1] == 0 || Matrix[i][j] == Matrix[i][j + 1]))
             {
-                Matrix[i][j+1]+=Matrix[i][j];
-                Matrix[i][j]=0;
+                Matrix[i][j + 1] += Matrix[i][j];
+                Matrix[i][j] = 0;
                 j++;
             }
             j--;
@@ -274,24 +343,24 @@ void Right()
 }
 void Left()
 {
-    int i,j;
-    for(i=0;i<SIZE;i++)
+    int i, j;
+    for (i = 0; i < SIZE; i++)
     {
-        j=1;
-        while(1)
+        j = 1;
+        while (1)
         {
-            while(Matrix[i][j]==0)
+            while (Matrix[i][j] == 0)
             {
-                if(j==SIZE)
+                if (j == SIZE)
                     break;
                 j++;
             }
-            if(j==SIZE)
+            if (j == SIZE)
                 break;
-            while(j>0 && (Matrix[i][j-1]==0 || Matrix[i][j]==Matrix[i][j-1]))
+            while (j > 0 && (Matrix[i][j - 1] == 0 || Matrix[i][j] == Matrix[i][j - 1]))
             {
-                Matrix[i][j-1]+=Matrix[i][j];
-                Matrix[i][j]=0;
+                Matrix[i][j - 1] += Matrix[i][j];
+                Matrix[i][j] = 0;
                 j--;
             }
             j++;
@@ -303,28 +372,31 @@ main()
 {
     int aro;
     char Arrow;
-    char s[]="THANKS FOR PLAYING, GOOD LUCK FOR NEXT TIME ";
+    char s[] = "THANKS FOR PLAYING, GOOD LUCK FOR NEXT TIME ";
     int temp;
     starting();
     Starting_Random();
-    Arrow=DOWN;
-    while(Arrow!=ESC)
+    Arrow = DOWN;
+    while (Arrow != ESC) //  HERE IF USER WANT TO EXIT THEN PRESS ESC KEY
     {
-        Arrow=getch();
-        aro=Arrow;
+        Arrow = getch();
+        aro = Arrow;
         Action(Arrow);
-        temp=Temp_counter();
-        if(temp==1)
+        temp = Temp_counter();
+        if (temp == 1)
         {
             printf("\n\t\t\tYOU WON");
+            ending();
             break;
         }
-        if(temp==0 || score<0)
+        if (temp == 0 || score < 0)
         {
             printf("\n\t\t\tSORRY !  GAME OVER\n");
+            ending();
             break;
         }
     }
+
     system("cls");
     printf("\n\n\n\t");
     printer(s);
